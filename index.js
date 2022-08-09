30482cecbd3a8e6ca991a409fb41a25d9b4756bb
secretj <qkrwlsgud890@gmail.com>
2022-08-10 00:21:01 +0900

    Node.js를 이용한 web server구축

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HomePage from './views/homePage.js';
+
+ReactDOM.render(
+    <HomePage />,
+    document.getElementById('root')
+);
